d9acb93cb7251e2d703564d3ccfcb29187f61083
Jitesh Bawaskar <jiteshbawaskar05@gmail.com>
2026-02-11 12:32:14 +0530

    write configuration

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,16 @@
+import os
 from pydantic import BaseModel
 from functools import lru_cache
 
 
 class Settings(BaseModel):
-    MONGO_URI: str = "mongodb://localhost:27017"
-    DATABASE_NAME: str = "online_bookstore"
-    SECRET_KEY: str = "supersecretkey"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    MONGO_URI: str = os.getenv("MONGO_URI")
+    DATABASE_NAME: str = os.getenv("DATABASE_NAME", "online_bookstore")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+    )
 
 
 @lru_cache()
