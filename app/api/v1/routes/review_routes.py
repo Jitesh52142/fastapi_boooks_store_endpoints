7f2bab73b7b97c209208634207dad9444d6f5312
Jitesh Bawaskar <jiteshbawaskar05@gmail.com>
2026-02-11 12:30:25 +0530

    implement review logic

--- app/api/v1/routes/review_routes.py
+++ app/api/v1/routes/review_routes.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, Depends
 from app.core.security import get_current_user
 from app.db.mongodb import db
 from datetime import datetime
+from app.core.exceptions import BadRequestException
+
+
 
 router = APIRouter(prefix="/api/v1/reviews", tags=["Reviews & Ratings"])
 
@@ -19,3 +22,34 @@ async def submit_review(book_id: str, rating: int, comment: str, user=Depends(ge
     await db.reviews.insert_one(review)
 
     return {"message": "Review submitted"}
+
+
+
+
+
+@router.post("/{book_id}")
+async def submit_review(book_id: str, rating: int, comment: str, user=Depends(get_current_user)):
+    if rating < 1 or rating > 5:
+        raise BadRequestException("Rating must be between 1 and 5")
+
+    review = {
+        "book_id": book_id,
+        "user_id": str(user["_id"]),
+        "rating": rating,
+        "comment": comment
+    }
+
+    await db.reviews.insert_one(review)
+
+    return {"message": "Review submitted successfully"}
+
+
+@router.get("/{book_id}")
+async def get_reviews(book_id: str):
+    reviews = []
+    async for review in db.reviews.find({"book_id": book_id}):
+        review["id"] = str(review["_id"])
+        del review["_id"]
+        reviews.append(review)
+
+    return reviews
