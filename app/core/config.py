4f3d49c29c478530072d9600c251f5e01796ff2c
Jitesh Bawaskar <jiteshbawaskar05@gmail.com>
2026-02-11 14:51:02 +0530

    update data base error mongo uri

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,10 @@
 import os
 from pydantic import BaseModel
 from functools import lru_cache
+from dotenv import load_dotenv
 
+# Load .env file
+load_dotenv()
 
 class Settings(BaseModel):
     MONGO_URI: str = os.getenv("MONGO_URI")
@@ -11,7 +14,6 @@ class Settings(BaseModel):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
         os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
     )
-
 
 @lru_cache()
 def get_settings():
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,17 +1,22 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
+
 
 class UserRegister(BaseModel):
     name: str
     email: EmailStr
-    password: str
+    password: str = Field(..., min_length=6, max_length=72)
 
+    
 class UserLogin(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(..., min_length=6, max_length=72)
+
 
 class UserResponse(BaseModel):
     id: str
     name: str
     email: EmailStr
     role: str
+
+
