acce91a59a8324199e36fc2fa112ee86e7cae8f5
Jitesh Bawaskar <jiteshbawaskar05@gmail.com>
2026-02-11 11:32:05 +0530

    implement review logic and routs

--- app/api/v1/routes/review_routes.py
+++ app/api/v1/routes/review_routes.py
@@ -1,1 +1,21 @@
-# review_routes.py
+from fastapi import APIRouter, Depends
+from app.core.security import get_current_user
+from app.db.mongodb import db
+from datetime import datetime
+
+router = APIRouter(prefix="/api/v1/reviews", tags=["Reviews & Ratings"])
+
+
+@router.post("/{book_id}")
+async def submit_review(book_id: str, rating: int, comment: str, user=Depends(get_current_user)):
+    review = {
+        "book_id": book_id,
+        "user_id": str(user["_id"]),
+        "rating": rating,
+        "comment": comment,
+        "created_at": datetime.utcnow()
+    }
+
+    await db.reviews.insert_one(review)
+
+    return {"message": "Review submitted"}
